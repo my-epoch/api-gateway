42d9cfa4b43e90ade98404bb4e9e6243ceb5e41b
ShuffleBoy <shuffle.almaz@gmail.com>
2022-06-06 11:56:58 +0500

    fix: import path for consul_client

--- cmd/gateway_server/main.go
+++ cmd/gateway_server/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/my-epoch/api-gateway/internal/gateway"
-	"github.com/my-epoch/api-gateway/pkg/consul"
+	"github.com/my-epoch/api-gateway/pkg/consul_client"
 	"github.com/my-epoch/api-gateway/pkg/logger"
 )
 
 func main() {
 	logger.Info("initializing")
-	consul.InitClient()
+	consul_client.InitClient()
 
 	gateway.Init()
 	gateway.Serve()
