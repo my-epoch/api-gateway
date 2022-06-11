57e5a050f96bd29a0917cd90dfda4af761c82583
ShuffleBoy <shuffle.almaz@gmail.com>
2022-06-11 23:42:55 +0500

    ref: deleted proto definitions, all definitions moved to service side

--- internal/gateway/services.go
+++ internal/gateway/services.go
@@ -3,7 +3,8 @@ package gateway
 import (
 	"context"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	service "github.com/my-epoch/api-gateway/gen/go/api/proto/v1"
+	objectService "github.com/my-epoch/object_service/gen/go/api/proto/v1"
+
 	"github.com/my-epoch/api-gateway/pkg/logger"
 	"google.golang.org/grpc"
 )
@@ -13,7 +14,7 @@ func registerServices(mux *runtime.ServeMux, ctx context.Context, opts []grpc.Di
 }
 
 func registerObjectService(mux *runtime.ServeMux, ctx context.Context, opts []grpc.DialOption) {
-	if err := service.RegisterObjectServiceHandlerFromEndpoint(ctx, mux, "object.service.consul:50050", opts); err != nil {
+	if err := objectService.RegisterObjectServiceAPIHandlerFromEndpoint(ctx, mux, "object.service.consul:50050", opts); err != nil {
 		logger.Warnf("cannot register object service: %e", err)
 		return
 	}
